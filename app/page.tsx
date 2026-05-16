96451badf44f7f6a993e057d912fd177952b3f5e
Virgel <virgelcortel@gmail.com>
2026-05-16 14:44:57 +0800

    implemented stripe

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
 import Image from "next/image";
+import {stripe} from "@/lib/stripe";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+
+  const products = await stripe.products.list({
+    expand:["data.default_price"],
+    limit: 5,
+  })
+
+  console.log(products)
   return (
     <div>
-
+      <section>
+        <div>
+          <div>
+            <h2> Welcome to SHop.Now</h2>
+            <p>All you need, we have it here!</p>
+            <Button asChild variant="default" className='rounded-full px-4 py-2'>
+              <Link href='/products' className="hover:text-blue-600">Browse All Products</Link>
+            </Button>
+          </div>
+          <Image src={products.data[0].images[0]} alt='Banner Image' width={450} height={450} />
+        </div>
+      </section>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
     return (
         <nav className ="sticky top-0 z-50 bg-white shadow">
             <div className="container mx-auto flex items-center justify-between px-4 py-4">
-                <Link href='/' className="hover:text-blue-600">Ecommerce</Link>
+                <Link href='/' className="hover:text-blue-600">SHop.Now</Link>
 
             <div className="hidden md:flex space-x-6">
                 <Link href='/' className="hover:text-blue-600">Home</Link>
